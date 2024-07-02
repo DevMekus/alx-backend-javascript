7ef96565adf8b9d9f06c805875485ac6fba4d9e7
Devmekus <nnaji.nnaemeka712@gmail.com>
2024-07-02 12:52:08 +0000

    Advanced task addes

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,14 @@
+/**
+ * Changes the quantity of unique 
+ * @param {Map<String, number>} map - A map of the name 
+ */
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+  map.forEach((value, key) => {
+    if (value === 1) {
+      map.set(key, 100);
+    }
+  });
+}
